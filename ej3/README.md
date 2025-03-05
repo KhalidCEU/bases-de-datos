d8b5462cb08fb180a3036ea12cfe67f451b78159
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2025-03-05 18:07:52 +0100

    feat: add exercise 3.1

---
+++ ej3/README.md
@@ -0,0 +1,5 @@
+## Empleados & Departamentos
+
+Ejercicio 3.1 del PDF de ejercicios
+
+<img width="471" src="https://github.com/user-attachments/assets/17173dbf-cb90-45c9-9733-3af4e91eaccb"/>
---
+++ ej3/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO Departamentos VALUES
+(1, 'Management', 'Madrid'),
+(2, 'HR', 'New York'),
+(3, 'R&D', 'Burgos'),
+(4, 'Coffee Central', 'Madrid'),
+(5, 'Garbage Collection', 'Burgos');
+
+INSERT INTO Empleados VALUES
+(1, 'Lorenzo', NULL, 10000, 1000, 'CEO', 1),
+(2, 'Manola', 1, 1000, 10000, 'CTO', 3),
+(3, 'Polnareff', 1, 5000, 5000, 'Head of HR', 2),
+(4, 'Zeppeli', 2, 1500, 8000, 'Head of R&D', 3),
+(5, 'Eustaquio', 4, 500, 0, 'Intern', 3),
+(6, 'Bernarda', 5, 0, 150, 'Interns Intern', 3),
+(7, 'Herminio', 2, 600, 600, 'Coffee Guy', 4);
